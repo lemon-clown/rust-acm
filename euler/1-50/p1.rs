7cf121243462941e63390c1ba8df938ee20ad663
lemon-clown <me.clown.lemon@gmail.com>
2020-05-19 20:01:47 +0800

    :pencil:  [eluer] doc: update comment docs

--- euler/1-50/p1.rs
+++ euler/1-50/p1.rs
@@ -1,20 +1,21 @@
-/**
- * If we list all the natural numbers below 10 that are multiples of 3 or 5,
- * we get 3, 5, 6 and 9. The sum of these multiples is 23.
- *
- * Find the sum of all the multiples of 3 or 5 below 1000.
- *
- * @see https://projecteuler.net/problem=1
- *
- * # 题目描述
- * 计算 [0, 1000) 中所有能被 3 或 5 整除的整数的和
- *
- * # 题解
- * 暴力枚举
- *
- * # 答案
- * 233168
- */
+//!
+//! # Problem Description
+//! If we list all the natural numbers below 10 that are multiples of 3 or 5,
+//! we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//!
+//! Find the sum of all the multiples of 3 or 5 below 1000.
+//!
+//! @see https://projecteuler.net/problem=1
+//!
+//! # 题目描述
+//! 计算 [0, 1000) 中所有能被 3 或 5 整除的整数的和
+//!
+//! # 题解
+//! 暴力枚举
+//!
+//! # 答案
+//! 233168
+//!
 fn main () {
   let mut answer = 0;
   for x in 0..1000 {
